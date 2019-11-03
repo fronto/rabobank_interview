e50875d581de61e06458da93edf49c8bffbb25fa
thomas.fitzpatrick <thomas4java@gmail.com|>
2019-11-03 21:54:11 +0100

    New empty maven module for persistence layer

--- person-data-web/src/main/java/nl/rabobank/PersonController.java
+++ person-data-web/src/main/java/nl/rabobank/PersonController.java
@@ -13,8 +13,6 @@ public class PersonController {
 
         Person person = parseDomainObjectFromDto(personDto);
 
-
-
         throw new UnsupportedOperationException("not implemented yet");
     }
 
