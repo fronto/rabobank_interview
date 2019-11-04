db654ae7d03117e6c2cc728b7573449310e7f45b
thomas.fitzpatrick <thomas4java@gmail.com|>
2019-11-04 20:41:34 +0100

    Make creation with name check an atomic operation
    In order to do this we have to enbale transaction
    management, and may as well enable jpa repositories
    at the application config level (as opposed to the
    integration test config level)

--- person-data-domain/src/main/java/nl/rabobank/interview/domain/PersonService.java
+++ person-data-domain/src/main/java/nl/rabobank/interview/domain/PersonService.java
@@ -3,13 +3,16 @@ package nl.rabobank.interview.domain;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 @Service
 @AllArgsConstructor
 public class PersonService {
 
     private final PersonRepository personRepository;
 
-    //TODO think about transaction demarcation
+    //TODO think more deeply about transaction demarcation
+    @Transactional
     public Person createNewPerson(Person person) {
 
         if(personRepository.hasPerson(person.getFirstName(), person.getLastName())) {
--- person-data-web/src/main/java/nl/rabobank/Application.java
+++ person-data-web/src/main/java/nl/rabobank/Application.java
@@ -5,10 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import java.util.Arrays;
 
 @SpringBootApplication
+@EnableTransactionManagement
+@EnableJpaRepositories
 public class Application {
 
 
