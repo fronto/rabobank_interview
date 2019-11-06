9292ae16012b16e7c459390ec3b07259970b17cb
thomas.fitzpatrick <thomas4java@gmail.com|>
2019-11-06 16:48:46 +0100

    Print out database url instead of bean names on start-up

--- person-data-web/src/main/java/nl/rabobank/Application.java
+++ person-data-web/src/main/java/nl/rabobank/Application.java
@@ -3,6 +3,7 @@ package nl.rabobank;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -24,13 +25,9 @@ public class Application {
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
 
-            System.out.println("Print beans provided by Spring Boot:");
-
-            String[] beanNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanNames);
-            for (String beanName : beanNames) {
-                System.out.println(beanName);
-            }
+            DataSourceProperties dataSource = ctx.getBean(DataSourceProperties.class);
+            String url = dataSource.getUrl();
+            System.out.println(String.format("Current database url is:  %s", url));
 
         };
     }
